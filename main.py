05bb8e1a0db407569f81934974740db61e8e9c73
Salman <s.z.gassem@gmail.com>
2024-09-01 21:37:16 +0300

    fixing model

--- main.py
+++ main.py
@@ -10,24 +10,27 @@ scaler_kmeans = joblib.load('kmens_scaler.joblib')
 
 # Define the data model for the input
 class InputFeatures(BaseModel):
-    provider: str
-    pevel: str
-    type_: str
-    duration_Weeks: str
+    rating : float
+    provider: int
+    level: int
+    reviews: int
+    course_type: int
+    duration_weeks: int
 
 # Function to preprocess the input data
 def preprocess_features(input_features: InputFeatures):
     dict_f = {
+        'rating': input_features.rating,
         'provider': input_features.provider,
         'level': input_features.level,
-        'type_': input_features.type_,
-        'duration_Weeks': input_features.duration_Weeks
+        'reviews': input_features.reviews,
+        'type': input_features.course_type,
+        'duration_Weeks': input_features.duration_weeks
     }
     # Convert dictionary values to a list in the correct order
     features_list = [dict_f[key] for key in sorted(dict_f)]
-    # Scale the input features
-    scaled_features = scaler_kmeans.transform([features_list])
-    return scaled_features
+
+    return [features_list]
 
 # Prediction endpoint
 @app.post("/predict")
