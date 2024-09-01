4afa151b87e02b53a32744f4f40cf4249199cadb
Salman <s.z.gassem@gmail.com>
2024-09-01 21:53:46 +0300

    fixing api

--- main.py
+++ main.py
@@ -10,37 +10,25 @@ scaler_kmeans = joblib.load('kmens_scaler.joblib')
 
 # Define the data model for the input
 class InputFeatures(BaseModel):
-<<<<<<< HEAD
     rating : float
     provider: int
     level: int
     reviews: int
     course_type: int
     duration_weeks: int
-=======
-    Provider: str
-    Level: str
-    Type: str
-    Duration_Weeks: str
->>>>>>> 8676b941a0f08e5518a91909afc0dd97ebd5a37d
+
 
 # Function to preprocess the input data
 def preprocess_features(input_features: InputFeatures):
     dict_f = {
-<<<<<<< HEAD
         'rating': input_features.rating,
         'provider': input_features.provider,
         'level': input_features.level,
         'reviews': input_features.reviews,
         'type': input_features.course_type,
         'duration_Weeks': input_features.duration_weeks
-=======
-        'Provider': input_features.Provider,
-        'Level': input_features.Level,
-        'Type': input_features.Type,
-        'Duration_Weeks': input_features.Duration_Weeks
->>>>>>> 8676b941a0f08e5518a91909afc0dd97ebd5a37d
     }
+
     # Convert dictionary values to a list in the correct order
     features_list = [dict_f[key] for key in sorted(dict_f)]
 
